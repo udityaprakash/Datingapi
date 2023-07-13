af5564712cbd7fd629d72df6bd84e13ed8ea3283
udityaprakash <udityaprakash983@gmail.com>
2023-07-14 01:56:15 +0530

    removed wastefull comments from algo js file

--- AdminPanel/home/match.js
+++ AdminPanel/home/match.js
@@ -27,68 +27,8 @@ async function getfemaleoptions(id){
     return false;
 }
 
-async function getmaleoptions(instaid){
-    
-}
-
-// async function getinstaid(user , priority){
-//     let u = await student.findOne({instauname:user,verified:true ,gender:true}).select(['options']);
-//     // prt(u);
-//     for(let m = 0 ; m<u.options.length; m++){
-
-//         if(u.options[m].priority == priority){
-//             // prt(u.options[m].instaid+' is this person we looked for--------------');
-//             return u.options[m].instaid;
-//         }
-//     }
-//     // return 0;
-// }
-
 async function  getallverifiedfemaleuser(){
     return await student.find({'matchedwith.name':null,'matchedwith.instaid':null, verified:true , gender: true}).select(['_id','instauname','fullname']);
-}
-async function  getmaleuserdetails(instaid){
-    const exist  = await student.findOne({instauname:instaid,verified:true, gender:false});
-    // prt("exuistance of user "+exist);
-    if(exist){
-
-        return exist;
-        // for(let j =0;j<exist.options.length; j++){
-        //     if(exist.options[j].instaid == femaleinstaid){
-
-        //         return true;
-        //     }
-        // }
-
-        // console.log("here this the user "+exist);
-    }
-    return false;
-}
-async function match(){
-    const females = await getallverifiedfemaleuser();
-    // prt(females);
-    for(let instance = 0; instance < females.length; instance++){
-
-        for (let c = 0; c <
-            females[instance].options.length
-            // 2
-            ; c++ ){
-            let id = await getinstaid(females[instance].instauname, c);
-    
-            // prt(`${id} printed here above`);
-            let call = await existverifiedmaleuser(id,females[instance].instauname);
-            console.log("here we get male persons option"+call);
-            if(call){
-                console.log("...............next............");
-                break ;
-            }
-        }
-        // if( call && call.){
-            
-        // }
-    }
-    // females.forEach(async (female)=>{
-    // });
 }
 
 //this will be replaced with database in future
