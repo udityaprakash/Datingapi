46b4e5b94ce469207142eb94f01a0ca810cd7c8f
udityaprakash <udityaprakash983@gmail.com>
2023-07-14 01:07:23 +0530

    changed everything according to new algorithm

--- AdminPanel/home/match.js
+++ AdminPanel/home/match.js
@@ -3,32 +3,62 @@ function prt(string){
     console.log(string);
 }
 
-async function getinstaid(user , priority){
-    let u = await student.findOne({instauname:user,verified:true ,gender:true}).select(['options']);
-    // prt(u);
-    for(let m = 0 ; m<u.options.length; m++){
-
-        if(u.options[m].priority == priority){
-            // prt(u.options[m].instaid+' is this person we looked for--------------');
-            return u.options[m].instaid;
+async function existandisfree(instaid,femaleinsta){
+    var k = await student.findOne({instauname:instaid,'matchedwith.name':null, verified:true, gender: false}).select(['options']);
+    if(k && k.options.length!=0){
+        var mk = k.options.findIndex(x => x.instaid === femaleinsta);
+        if(mk==-1){
+            prt("its -1");
+            return false;
         }
+        else{
+            return true;
+        }
+        
     }
-    // return 0;
+    return false;
 }
+
+async function getfemaleoptions(id){
+    var k = await student.findOne({_id:id, verified:true , gender: true}).select(['options']);
+    if(k){
+        return k;
+    }
+    return false;
+}
+
+async function getmaleoptions(instaid){
+    
+}
+
+// async function getinstaid(user , priority){
+//     let u = await student.findOne({instauname:user,verified:true ,gender:true}).select(['options']);
+//     // prt(u);
+//     for(let m = 0 ; m<u.options.length; m++){
+
+//         if(u.options[m].priority == priority){
+//             // prt(u.options[m].instaid+' is this person we looked for--------------');
+//             return u.options[m].instaid;
+//         }
+//     }
+//     // return 0;
+// }
 
 async function  getallverifiedfemaleuser(){
-    return await student.find({verified:true , gender: true}).select(['instauname','fullname','options']);
+    return await student.find({verified:true , gender: true}).select(['_id','instauname']);
 }
-async function  existverifiedmaleuser(instauname,femaleinstaid){
-    const exist  = await student.findOne({instauname:instauname ,verified:true , gender: false}).select('options');
+async function  getmaleuserdetails(instaid){
+    const exist  = await student.findOne({instauname:instaid,verified:true, gender:false});
     // prt("exuistance of user "+exist);
     if(exist){
-        for(let j =0;j<exist.options.length; j++){
-            if(exist.options[j].instaid == femaleinstaid){
 
-                return true;
-            }
-        }
+        return exist;
+        // for(let j =0;j<exist.options.length; j++){
+        //     if(exist.options[j].instaid == femaleinstaid){
+
+        //         return true;
+        //     }
+        // }
 
         // console.log("here this the user "+exist);
     }
@@ -73,12 +103,32 @@ const result ={
         if(username && password){
 
             if(username == Admin.username && password == Admin.password){
-                await match();
-                
+                // await match();
+                const Allfemaleids = await getallverifiedfemaleuser();
+                // prt(Allfemaleids);  
+                for(var i=0;i<1 // Allfemaleids.length
+                    ;i++){
+                    var userdetail = await getfemaleoptions(Allfemaleids[i]._id);
+                    let pri = 0;
+                    // prt(userdetail);
+                    var j=0;
+                    for(j=0;j<1  //userdetail.options.length
+                    ;j++){
+                        var ins = userdetail.options.findIndex(x => x.priority == j);
+                        // prt(ins);
+                        // prt(userdetail.options[ins].instaid+"  ---  "+Allfemaleids[i].instauname);
+                        // prt(await existandisfree(userdetail.options[ins].instaid, Allfemaleids[i].instauname));
+                        if(await existandisfree(userdetail.options[ins].instaid),Allfemaleids[i].instauname){
+                            var m = student.findByIdAndUpdate(Allfemaleids[i]._id,{'matchedwith.name':'hella'});
+                            break;
+                        }
+                    }
+                }
+
                 res.json({
                     success:true,
                     msg:"setting up",
-                    // data:await match()
+                    data: (Allfemaleids.length > 0) ? Allfemaleids : 'Empty'
                 });
             }else{
                 res.json({success:false,msg:"Invalid Admin"});
